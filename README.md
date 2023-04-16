fda1780968bfa24d014cd6b79060dfc755812194
misaka10843 <misaka10843@outlook.jp>
2023-04-16 15:50:32 +0800

    第一次提交

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # Live-record-script
-Used to extend the functional scripts in olive (the scripts in this repository can be used mostly separately)
+Used to extend the functional scripts in [olive](https://github.com/go-olive/olive) (the scripts in this repository can be used mostly separately)
+
+用于扩展[olive](https://github.com/go-olive/olive)中的功能脚本(此仓库的脚本基本上都能分开独立使用)
+
+此仓库中的所有脚本均为为了让olive拥有更多自定义的上传/通知功能
+
+如果是olive本身的问题，还请不要在此仓库发送Issue
+
+## Attention! （中文用户可以跳过）
+
+Please note that since most of the functions in this repository are designed for uploading live recordings to Bilibili or for personal use, other websites or languages besides Chinese have not been taken into consideration.
+
+## 使用前必看
+
+因为编写此仓库时考虑到可能有人只需要特定一个功能，所有并没有将所有功能集成
+
+所以您需要自己编写`config.yaml`，还有按照下文安装依赖(如果懒或者需要全部功能可以直接`pip install -r requirements.txt`)
+
+`config.yaml`格式如下
+
+```yaml
+# mail.py配置
+mail:
+  # 邮箱的smtp服务器
+  mail_host: "smtp.163.com"
+  # 您的邮箱
+  mail_user: "xxxxxxx@163.com"
+  # smtp的密钥。基本上国内的邮箱都不是登录密码，而是需要申请的key
+  mail_pass: "xxxxxxx"
+  # 发送邮箱，最好与mail_user相同
+  sender: "xxxxxxx@163.com"
+  # 收件人邮箱，需要将提示邮件发送给谁，可以以数组的方式输入
+  receivers: ['xxxxxxx@outlook.jp','xxxx@qq.com']
+```
---
+++ mail.py
@@ -0,0 +1,65 @@
+import os
+import smtplib
+from email.header import Header
+from email.mime.text import MIMEText
+import yaml
+import sys
+
+if not os.path.exists("config.yaml"):
+    print("您并没有config.yaml，请按照GitHub仓库中的README进行设置！")
+    exit()
+with open('config.yaml', 'r') as file:
+    data = yaml.load(file, Loader=yaml.FullLoader)
+print(data)
+print(sys.argv)
+
+if len(sys.argv) == 1:
+    print("缺少主播ID arg或者文件路径arg，请按照GitHub仓库中的README进行运行！")
+    exit()
+Streamer = sys.argv[1]
+FILE_PATH = sys.argv[2]
+
+# 第三方 SMTP 服务
+mail_host = data['mail']['mail_host']  # SMTP服务器
+mail_user = data['mail']['mail_user']  # 用户名
+mail_pass = data['mail']['mail_pass']  # 授权密码，非登录密码
+
+sender = data['mail']['sender']  # 发件人邮箱(最好写全, 不然会失败)
+receivers = data['mail']['receivers']  # 接收邮件，可设置为你的QQ邮箱或者其他邮箱
+
+content = f'{Streamer}有一个直播录像已完成，还请注意！<br>文件路径为：{FILE_PATH}'
+title = f'{Streamer}有一个直播录像已完成！'  # 邮件主题
+
+
+def sendEmail():
+    message = MIMEText(content, 'plain', 'utf-8')  # 内容, 格式, 编码
+    message['From'] = "{}".format(sender)
+    message['To'] = ",".join(receivers)
+    message['Subject'] = title
+
+    try:
+        smtpObj = smtplib.SMTP_SSL(mail_host, 465)  # 启用SSL发信, 端口一般是465
+        smtpObj.login(mail_user, mail_pass)  # 登录验证
+        smtpObj.sendmail(sender, receivers, message.as_string())  # 发送
+        print("mail has been send successfully.")
+    except smtplib.SMTPException as e:
+        print(e)
+
+
+def send_email2(SMTP_host, from_account, from_passwd, to_account, subject, content):
+    email_client = smtplib.SMTP(SMTP_host)
+    email_client.login(from_account, from_passwd)
+    # create msg
+    msg = MIMEText(content, 'plain', 'utf-8')
+    msg['Subject'] = Header(subject, 'utf-8')  # subject
+    msg['From'] = from_account
+    msg['To'] = to_account
+    email_client.sendmail(from_account, to_account, msg.as_string())
+
+    email_client.quit()
+
+
+if __name__ == '__main__':
+    print(title)
+    print(content)
+    # sendEmail()
